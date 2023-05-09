fd07238f5d95fdc4067530f1b7778264db517961
Nick <nickigber@gmail.com>
2023-05-09 08:30:43 +0100

    fix issues

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-
-# Accept the argument from the command line
-input = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /School/
-
-# Pass the input to the regular expression matching method
-result = pattern.match(input)
-
-# Print the result
-if result
-  puts result[0]
-else
-  puts ""
-end
-
+puts ARGV[0].scan(/School/).join
